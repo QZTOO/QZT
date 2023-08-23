d50686e546488bbcdb70914a6efa4ee15caad07f
QZTOO <ziru@stumail.ysu,edu.cn>
2023-08-23 17:08:03 +0800

    first commit

---
+++ test_8_22/test.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+//练习题
+//#include<string.h>
+//int main()
+//{
+//	int i = 0;
+//	char password[20] = { 0 };
+//	for (i = 0; i < 3; i++)
+//	{
+//		printf("请输入密码:>");
+//		scanf("%s", password);
+//		if (strcmp(password, "123456") == 0)
+//		{
+//			printf("登录成功\n");
+//			break;
+//		}
+//		else
+//		{
+//
+//	}	printf("密码错误\n");
+//		}
+//	if (3 == i)
+//		printf("三次密码均错误，退出程序\n");
+//	return 0;
+//}
